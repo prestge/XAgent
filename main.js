14103e844eec561c7ef26e8223a152e5447671b9
prestge <296708066+prestge@users.noreply.github.com>
2026-07-23 22:45:13 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+/**
+ * @description initialize NW window.
+ */
+    nw.Window.open('index.html', {
+        width: 800,
+        height: 600,
+        focus: true,
+        show: true,
+        resizable: true,
+        icon: "icon.svg",
+        position: "center",
+
+
+    }
+);
